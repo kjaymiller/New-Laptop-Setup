2f59e2e972dff222e86fededa97d45d6540f759c
Jay Miller <kjaymiller@gmail.com>
2016-06-10 23:49:57 -0700

    Add files via upload

---
+++ start_script.sh
@@ -0,0 +1,19 @@
+# Installed Homebrew casks python and git
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew install python3
+brew install git
+brew install eo
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
+
+brew install exercism
+brew casks install dropbox
+brew cask install 1password
+brew cask install iterm2
+#brew cask install textexpander #Still Installing the Old Version
+brew cask install alfred
+brew cask install fission
+brew cask install audio-hijack
+brew cask install firefox
+
+
